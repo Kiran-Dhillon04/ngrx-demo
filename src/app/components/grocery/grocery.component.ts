88f91684ebb4931556e198d140ece947979b2f90
Kiran-Dhillon04 <kiranpreetkaurdhillon04@gmail.com>
2025-09-24 23:04:34 +0530

    Initial commit with configuration

---
+++ src/app/components/grocery/grocery.component.ts
@@ -0,0 +1,49 @@
+import { Component, Signal } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Grocery } from '../../../models/grocery.model';
+import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+
+
+@Component({
+  selector: 'app-grocery',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './grocery.component.html',
+  styleUrl: './grocery.component.css'
+})
+export class GroceryComponent {
+  groceries$?: Observable<Grocery[]>;
+  
+  constructor(private store: Store<{groceries:Grocery[]} >) {
+   this.groceries$= store.select("groceries")
+  }
+  
+
+
+
+  onTypeChange(event: Event){
+
+  }
+
+
+  increment(item:Grocery){
+    const payload = {
+      id:item.id,
+      name:item.name,
+      quantity:1
+    }
+
+
+  }
+  decrement(item:Grocery){
+    const payload = {
+      id:item.id,
+      name:item.name
+    }
+
+
+
+  }
+
+}
